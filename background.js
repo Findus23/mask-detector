04bcbde06def33d7e0c221e4007595fbb1533ae8
Lukas Winkler <l.winkler@mailbox.org>
2017-09-22 11:08:12 +0200

    also support tv and show typo3 version

--- background.js
+++ background.js
@@ -1,71 +1,92 @@
-function enabled(tabId) {
-    chrome.pageAction.show(tabId);
+function displayData(data, tabId, typo3) {
+    var icon;
+    if (typo3) {
+        chrome.pageAction.show(tabId);
+        if (data.mask.found) {
+            icon = "icon";
+        } else if (data.tv.found) {
+            icon = "icon-tv";
+        } else if (typo3) {
+            icon = "icon-typo3";
+        } else {
+            icon = "icon-disabled";
+        }
+    } else {
+        chrome.pageAction.hide(tabId);
+        icon = "icon-disabled";
+    }
     chrome.pageAction.setIcon({
         tabId: tabId,
         path: {
-            "128": "logo.128.png"
+            "128": "img/" + icon + ".128.png"
         }
     })
 }
 
-function disabled(tabId) {
-    chrome.pageAction.hide(tabId);
-    chrome.pageAction.setIcon({
-        tabId: tabId,
-        path: {
-            "128": "logo-disabled.128.png"
-        }
-    })
-}
-function typo3(tabId) {
-    chrome.pageAction.show(tabId);
-    chrome.pageAction.setIcon({
-        tabId: tabId,
-        path: {
-            "128": "logo-typo3.128.png"
-        }
-    })
-}
-
-function handleMessage(request, sender, sendResponse) {
+function handleMessage(request, sender) {
+    console.log("recieved message");
     var tabId = sender.tab.id;
     if (request.typo3) {
         chrome.storage.local.get(request.url, function (result) {
             var data = result[request.url];
             if (!data || Date.now() - data.updated > 1000 * 60 * 60 * 24) {
+                data = {};
+
                 var xmlHttp = new XMLHttpRequest();
+                data.mask = {};
                 xmlHttp.onreadystatechange = function () {
-                    var found;
                     if (xmlHttp.readyState === 4) {
-                        if (xmlHttp.status === 200) {
-                            found = true;
-                            enabled(tabId);
-                        } else {
-                            found = false;
-                            typo3(tabId);
-                        }
+                        data.mask.found = xmlHttp.status === 200;
+                        data.mask.modified = xmlHttp.getResponseHeader('Last-Modified');
+                        var tvHttp = new XMLHttpRequest();
+                        data.tv = {};
+                        tvHttp.onreadystatechange = function () {
+                            if (tvHttp.readyState === 4) {
+                                data.tv.found = tvHttp.status === 200;
+                                data.tv.modified = tvHttp.getResponseHeader('Last-Modified');
+
+                                var cl = new XMLHttpRequest();
+                                cl.onreadystatechange = function () {
+                                    if (cl.readyState === 4) {
+                                        if (cl.status === 200) {
+                                            var changelog = cl.responseText.split('\n', 1)[0];
+                                            const regex = /(\d\.)+(\d+)/;
+                                            var match = regex.exec(changelog);
+                                            data.t3version = match[0];
+                                        }
+                                        console.log("saving data");
+                                        data.updated = Date.now();
+
+                                        displayData(data, tabId, true);
+
+                                        var storage = {};
+                                        storage[request.url] = data;
+                                        chrome.storage.local.set(storage);
+                                    }
+                                };
+
+                                cl.open("GET", request.url + "/typo3_src/ChangeLog", true);
+                                console.log("request changelog");
+                                cl.send();
+                            }
+                        };
+                        tvHttp.open("GET", request.url + "/typo3conf/ext/templavoila/ext_icon.gif", true);
+                        console.log("request tv icon");
+                        tvHttp.send();
                     }
-                    var data = {};
-                    data[request.url] = {
-                        found: found,
-                        modified: xmlHttp.getResponseHeader('Last-Modified'),
-                        updated: Date.now()
-                    };
-                    chrome.storage.local.set(data);
-                };
-                xmlHttp.open("GET", request.url + "/typo3conf/ext/mask/ext_icon.gif", true);
-                xmlHttp.send();
-            } else {
-                if (data.found) {
-                    enabled(tabId);
-                } else {
-                    typo3(tabId);
                 }
+                ;
+                xmlHttp.open("GET", request.url + "/typo3conf/ext/mask/ext_icon.gif", true);
+                console.log("request mask icon");
+                xmlHttp.send();
+            }
+            else {
+                displayData(data, tabId, true)
             }
         });
     }
     else {
-        disabled(tabId);
+        displayData(false, tabId, false)
     }
 }
 
--- popup.js
+++ popup.js
@@ -2,8 +2,19 @@ chrome.tabs.query({active: true}, function (tabs) {
     var url = new URL(tabs[0].url);
     var key = url.protocol + "//" + url.host;
     chrome.storage.local.get(key, function (result) {
-        if(result[key]) {
-            document.getElementById("lastModified").innerText = result[key].modified;
+        var text;
+        if (result[key]) {
+            if (result[key].mask.modified) {
+                text = result[key].mask.modified;
+            } else if (result[key].tv.modified) {
+                text = result[key].tv.modified
+            }
+        }
+        if (text) {
+            document.getElementById("lastModified").innerText = text;
+        }
+        if (result[key].t3version) {
+            document.getElementById("typo3Version").innerText = result[key].t3version;
         }
     });
 });
